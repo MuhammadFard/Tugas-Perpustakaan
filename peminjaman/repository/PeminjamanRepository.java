0b62cc8506f6e297afeb990a6d8baca4a8454d45
MuhammadFard <113502299+MuhammadFard@users.noreply.github.com>
2023-01-04 09:59:09 +0700

    Add files via upload

---
+++ peminjaman/repository/PeminjamanRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.farid.peminjaman.repository;
+
+import com.farid.peminjaman.entity.Peminjaman;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author IIISI
+ */
+@Repository
+public interface PeminjamanRepository extends JpaRepository<Peminjaman, Long>{
+    
+    public Peminjaman findByPeminjamanId(Long peminjamanId);
+}
